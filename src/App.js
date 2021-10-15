c01d8eefa2a67ad0a6c41c1690cdf1b1ed7ad17d
Ritesh <riteshaldak13022000@gmail.com>
2021-10-15 23:55:48 +0530

    use the Navbar as component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,10 @@
 import './App.css';
-
+import Navbar from './components/Navbar';   // also use this code to import Navbar component here
 function App() {
+  // this will equal to Navbar component code  --> <Navbar/>
   return (
     <>
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary" >
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">Textutils</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarText">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">About</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">Contact</a>
-        </li>
-      </ul>
-      <form className="d-flex">
-      <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button className="btn btn-outline-success bg-light " type="submit">Search</button>
-    </form>
-    </div>
-  </div>
-</nav>
-
-
-
-    
-    
-    
-    
+    <Navbar/> 
     </>
   );
 }
