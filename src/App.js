f76505934f4c5a2a39035b3a735f69419b901b93
Ritesh <riteshaldak13022000@gmail.com>
2021-10-15 22:24:08 +0530

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary" >
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">Textutils</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarText">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">About</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">Contact</a>
+        </li>
+      </ul>
+      <form className="d-flex">
+      <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button className="btn btn-outline-success bg-light " type="submit">Search</button>
+    </form>
     </div>
+  </div>
+</nav>
+
+
+
+    
+    
+    
+    
+    </>
   );
 }
 
