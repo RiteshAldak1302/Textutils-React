bcceeadfd1058b0755b6b233923cc1f2bce77758
Ritesh-Aldak <riteshaldak13022000@gmail.com>
2021-11-06 16:14:50 +0530

    change the href to /

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,7 +18,7 @@ export default function Navbar(props) {
     return (
         <nav className={`navbar navbar-expand-lg navbar-dark bg-primary`} >
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">{props.title}</a>
+          <a className="navbar-brand" href="/">{props.title}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -26,7 +26,7 @@ export default function Navbar(props) {
             
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                <a className="nav-link active" aria-current="page" href="/">Home</a>
               </li>
               {/* <li className="nav-item">
                 <Link className="nav-link" href="/about">About</Link>
